53c398ad50e9d21f31464ea9e8886e890680d711
Darek Wiktorowski <wiktorowski.dariusz1@gmail.com>
2025-02-06 21:31:58 +0100

    AnimatedContent - refactor, API changes

--- src/AnimatedContent.razor.cs
+++ src/AnimatedContent.razor.cs
@@ -14,7 +14,8 @@ public partial class AnimatedContent<TState>
     [Parameter]
     public RenderFragment<TState?>? ChildContent { get; set; }
     [Parameter]
-    public GetTransitions? TransitionsProvider { get; set; }
+    public Func<StateChange, InterstateTransitions>? TransitionsProvider { get; set; }
+
 
     [Parameter]
     public bool NewStateOnTop { get; set; }
@@ -36,10 +37,37 @@ public partial class AnimatedContent<TState>
     [Parameter]
     public ExitTransition? SharedExit { get; set; }
 
-    private List<StateRecord> PastStates { get; } = new();
-    private int CurrentStateKey { get; set; } = 0;
-    private bool HasInitialTargetStateBeenShown { get; set; } = false;
 
+    public readonly record struct StateChange(
+        TState? Source,
+        TState? Target);
+
+    public class InterstateTransitions
+    {
+        public EnterTransition? TargetEnter { get; init; }
+        public ExitTransition? SourceExit { get; init; }
+    }
+
+
+    public class StateSwitchCase
+    {
+        public required RenderFragment Fragment { get; init; }
+
+        public EnterTransition? Enter { get; init; }
+        public ExitTransition? Exit { get; init; }
+
+
+        public static implicit operator StateSwitchCase(RenderFragment fragment)
+            => new()
+            {
+                Fragment = fragment
+            };
+    }
+
+
+    private readonly List<StateRecord> _pastStates = new();
+    private int _currentStateKey = 0;
+    private bool _hasInitialTargetStateBeenShown = false;
     private bool _hasInitialParametersBeenSet = false;
 
     protected override void OnParametersSet()
@@ -49,9 +77,9 @@ public partial class AnimatedContent<TState>
         {
             if (_hasInitialParametersBeenSet)
             {
-                PastStates.Add(new StateRecord
+                _pastStates.Add(new StateRecord
                 {
-                    Key = CurrentStateKey++,
+                    Key = _currentStateKey++,
                     State = _targetState
                 });
             }
@@ -65,7 +93,7 @@ public partial class AnimatedContent<TState>
 
     protected override void OnAfterRender(bool firstRender)
     {
-        HasInitialTargetStateBeenShown = true;
+        _hasInitialTargetStateBeenShown = true;
     }
 
 
@@ -76,7 +104,7 @@ public partial class AnimatedContent<TState>
 
     private void OnPastStateElementWasHidden(StateRecord pastState)
     {
-        PastStates.Remove(pastState);
+        _pastStates.Remove(pastState);
     }
 
     private string ContainerStyles
@@ -96,32 +124,77 @@ public partial class AnimatedContent<TState>
         }
     }
 
+
     private readonly struct StateRecord
     {
         public required int Key { get; init; }
         public required TState? State { get; init; }
     }
 
-    public class StateSwitchCase
+    private AnimatedVisibilityParameters[] GetAnimatedVisibilityParametersForRender(Func<TState?, StateSwitchCase> getStateCase)
     {
-        public required RenderFragment Fragment { get; init; }
+        var paramsOfStatesToRender = new AnimatedVisibilityParameters[_pastStates.Count + 1];
 
-        public EnterTransition? Enter { get; init; }
-        public ExitTransition? Exit { get; init; }
+        var targetStateCase = getStateCase(TargetState);
+        var targetTransitions = TransitionsProvider?.Invoke(new StateChange(
+            Source: default,
+            Target: TargetState));
+
+        paramsOfStatesToRender[0] = new AnimatedVisibilityParameters(
+            Visible: true,
+            Enter: targetTransitions?.TargetEnter ?? targetStateCase.Enter ?? SharedEnter,
+            Exit: null, // exit is not relevant for target state, it will be set when becomes past state
+            StartWithTransition: StartWithTransition || _hasInitialTargetStateBeenShown,
+            OnHidden: default,
+            OnShown: new EventCallback(this, () => OnTargetStateElementWasShown()),
+            Key: _currentStateKey,
+            Fragment: targetStateCase.Fragment
+        );
 
 
-        public static implicit operator StateSwitchCase(RenderFragment fragment)
-            => new()
+        if (_pastStates.Count > 0)
+        {
+            var fromProcessedToNewerStateTransitions = TransitionsProvider?.Invoke(new StateChange(
+                Source: _pastStates[^1].State,
+                Target: TargetState));
+
+            for (int pastStateIndexFromEnd = 1; pastStateIndexFromEnd <= _pastStates.Count; pastStateIndexFromEnd++)
             {
-                Fragment = fragment
-            };
+                var pastState = _pastStates[^pastStateIndexFromEnd];
+                var pastStateCase = getStateCase(pastState.State);
+
+                var fromOlderToProcessedStateTranstions = TransitionsProvider?.Invoke(new StateChange(
+                    Source: pastStateIndexFromEnd + 1 <= _pastStates.Count ? _pastStates[^(pastStateIndexFromEnd + 1)].State : default,
+                    Target: _pastStates[^pastStateIndexFromEnd].State));
+
+                paramsOfStatesToRender[pastStateIndexFromEnd] = new AnimatedVisibilityParameters(
+                    Visible: false,
+                    Enter: fromOlderToProcessedStateTranstions?.TargetEnter ?? pastStateCase.Enter ?? SharedEnter,
+                    Exit: fromProcessedToNewerStateTransitions?.SourceExit ?? pastStateCase.Exit ?? SharedExit,
+                    StartWithTransition: false,
+                    OnHidden: new EventCallback(this, () => OnPastStateElementWasHidden(pastState)),
+                    OnShown: default,
+                    Key: pastState.Key,
+                    Fragment: pastStateCase.Fragment
+                );
+
+                // when processing next (older) state we can reuse already found transitions (older becomes processed, processed becomes newer)
+                fromProcessedToNewerStateTransitions = fromOlderToProcessedStateTranstions;
+            }
+        }
+
+        return paramsOfStatesToRender;
     }
 
-    public class InterstateTransitions
-    {
-        public EnterTransition? FromPreviousStateEnter { get; set; }
-        public ExitTransition? ToNextStateExit { get; set; }
-    }
 
-    public delegate InterstateTransitions? GetTransitions(TState? fromState, TState? toState);
+    private record AnimatedVisibilityParameters(
+        bool Visible,
+        EnterTransition? Enter,
+        ExitTransition? Exit,
+        bool StartWithTransition,
+        EventCallback OnHidden,
+        EventCallback OnShown,
+        int Key,
+        RenderFragment Fragment
+    );
 }
